f80bc1bace6c5aea03e7f6b3d3ba5236e69df293
Juju Man <hunteripad2@gmail.com>
2021-08-28 22:55:11 +0300

    Fixed production

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom"; // TODO: hash router
-import reportWebVitals from "./reportWebVitals";
 import "./styles.scss";
 import { App } from "./App";
 
